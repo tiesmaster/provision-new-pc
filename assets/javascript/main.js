3364d5505d1cf08ae88b69ba2531188e688c7b7e
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-05-03 21:57:57 +0200

    Move main javascript from head.html to main.js.

---
+++ assets/javascript/main.js
@@ -0,0 +1,23 @@
+$(function() {
+
+    var availableInstructionsDict = $('.post-list li').map(function () {
+        var li = $(this);
+        return { label: li.text().trim(), value: $('a.post-link', li).get(0) };
+    }).get();
+
+    var availableInstructions = availableInstructionsDict.map(function (x) { return x.label; });
+
+    $( "#quick-search-box" ).autocomplete({
+        source: availableInstructions,
+        select: function( event, ui ) { availableInstructionsDict.find(function(y) { return y.label == ui.item.label; }).value.click()}
+    });
+
+    $(document).bind('keyup', '/', function(){
+        $('#quick-search-box').focus();
+    });
+
+    $(document).bind('keyup', 'h', function(){
+        $('.site-title').get(0).click();
+    });
+
+});
